c07428d7222f4030e197b0aa3a428c27e5014687
KauaneBastosT <162719286+KauaneBastosT@users.noreply.github.com>
2024-03-12 11:23:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://ulbra-to.br/encena/uploads/tiara-de-cabelo-princesa-bela-tiara-de-cabelo.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://www.verberenas.com/wp-content/uploads/2016/03/belle-1.jpg.webp";
+}
